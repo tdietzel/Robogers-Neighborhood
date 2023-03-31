8f2166dce014f46eff88a825e1f18622ab6ec3ab
tdietzel <dietzelbiz@outlook.com>
2023-03-31 09:18:55 -0700

    WIP second test (JS)

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 //Business Logic
 
-function neighbor(number) {
+function neighbor(userInput) {
+  const number = userInput.trim();
+  console.log(number);
   if (!Number(number)) {
     return false;
   } else {
